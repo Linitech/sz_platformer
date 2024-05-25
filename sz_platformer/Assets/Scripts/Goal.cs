915000b8fe35a52666d68bd5518e5708eabd2c9f
Linitech <158758761+Linitech@users.noreply.github.com>
2024-05-24 21:46:19 -0400

    Update Goal.cs

--- sz_platformer/Assets/Scripts/Goal.cs
+++ sz_platformer/Assets/Scripts/Goal.cs
@@ -14,5 +14,16 @@ public class Goal : MonoBehaviour
         }
     }
 
+    // start is called before the first frame update
+    void Start()
+    {
     
+    }
+
+    // update is called once per frame
+    void Update
+    {
+
+    }
+
 }
